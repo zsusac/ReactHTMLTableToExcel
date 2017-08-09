b9b9404530ea446a21cae895746ec6ce86b7bfab
Lora Zulicek <lora.zulicek@fer.hr>
2017-08-09 16:17:02 +0200

    Fix build bug

--- src/ReactHTMLTableToExcelButton.jsx
+++ src/ReactHTMLTableToExcelButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import withReactHTMLTableToExcel from './ReactHTMLTableToExcel.jsx';
+import withReactHTMLTableToExcel from './ReactHTMLTableToExcel';
 
 const Button = (props) => (
      <button
