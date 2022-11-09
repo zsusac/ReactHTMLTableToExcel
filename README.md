22d4f816e7c759a1431c69ccadeb2cbacff5290e
Ayoola David <ayooladavid@gmail.com>
2022-11-09 15:04:38 +0100

    Update ReadMe and run prepublish.

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# ReactHTMLTableToExcel
-Provides a client side generation of Excel (.xls) file from HTML table element.
+# ReactHTMLTableToExcel V3
+Provides a client side generation of Excel (.xls/.xlsx) file from HTML table element.
+
 
 [![Build Status](https://travis-ci.org/zsusac/ReactHTMLTableToExcel.svg?branch=master)](https://travis-ci.org/zsusac/ReactHTMLTableToExcel)
 
-[![NPM](https://nodei.co/npm/react-html-table-to-excel.png)](https://npmjs.org/package/react-html-table-to-excel)
+[![NPM](https://nodei.co/npm/react-html-table-to-excel-3.png)](https://npmjs.org/package/react-html-table-to-excel-3)
 
 ___
 No additional dependencies
@@ -12,7 +13,7 @@ ___
 ## Installation
 
 ```
-npm install --save react-html-table-to-excel
+npm install --save react-html-table-to-excel-3
 ```
 
 ## Features
@@ -87,3 +88,7 @@ class Test extends Component {
 
 export default Test
 ```
+
+### NB: This repository is a super set of the original [ReactHTMLTableToExcel](https://npmjs.org/package/react-html-table-to-excel-3) package by [zsusac](https://github.com/zsusac) which is currently no longer maintained. This version adds some new features but maintains the original core.
+
+I do not retain the ownership or responsibility of this package.
--- index.js
+++ index.js
@@ -72,8 +72,8 @@ var ReactHTMLTableToExcel = function (_Component) {
 
       var table = document.getElementById(this.props.table).outerHTML;
       var sheet = String(this.props.sheet);
-      var filetype = String((this.props.filetype)?(this.props.filetype == 'xlsx'?'xlsx':'xls'):'xls')
-      var filename = String(this.props.filename) + '.' + filetype;
+      var filetype = String(this.props.filetype ? this.props.filetype === 'xlsx' ? 'xlsx' : 'xls' : 'xls');
+      var filename = '' + (String(this.props.filename) + '.' + filetype);
 
       var uri = 'data:application/vnd.ms-excel;base64,';
       var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-mic' + 'rosoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta cha' + 'rset="UTF-8"><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:Exce' + 'lWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/>' + '</x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></' + 'xml><![endif]--></head><body>{table}</body></html>';
@@ -114,7 +114,7 @@ var ReactHTMLTableToExcel = function (_Component) {
           type: 'button',
           onClick: this.handleDownload
         },
-        this.props.buttonText
+        this.props.children ? this.props.children : this.props.buttonText
       );
     }
   }], [{
